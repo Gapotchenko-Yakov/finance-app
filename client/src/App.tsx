09dc43cb1fe97809fe664762c5555d9f551aa21c
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-11 22:11:28 +0300

    Theme configuration.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
-  console.log("count", count, setCount);
-
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        App
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
